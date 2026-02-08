e9f69145700fc4162e3384d5a8a4c50583b0c86c
@StSachi <93354446+StSachi@users.noreply.github.com>
2026-02-08 18:00:41 +0100

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -1,1 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@sgse.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('admin123'),
+                'papel' => 'ADMIN',
+                'ativo' => true,
+            ]
+        );
+    }
+}
 
